ea61ac1ff90a79df2cbbd54b8abdcf92041e0880
Loic VASILE <loic.vasile@stagiaire-elan.fr>
2024-10-28 14:49:52 +0100

    Add comments

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function shuffleChildren(parent) { //
 
     while (--i > 0) { // while (i - 1) > 0
         k = Math.floor(Math.random() * (i + 1)); // k stocke un nombre aléatoire basé sur i
-        temp = board.children[k]; // temp pointe temporairement l'élément à la position k dans board
-        board.children[k] = board.children[i]; // remplace lélément à la position k par l'élément à la position i
+        temp = children[k]; // temp pointe temporairement l'élément à la position k dans board
+        children[k] = children[i]; // remplace lélément à la position k par l'élément à la position i
         board.appendChild(temp); // place l'élément k pointé temporairement à la fin du contenu de board
     }
 }
@@ -15,6 +15,7 @@ function showReaction(type, clickedBox) {
     clickedBox.classList.add(type);
 
     if (type != "success") {
+        // notice et error ne seront attribuées que pendant 0.8s
         setTimeout(function () {
             clickedBox.classList.remove(type);
         }, 800)
@@ -32,7 +33,10 @@ function timer() { // https://stackoverflow.com/a/31559606
     // begin countdown
     let timer = setInterval(function () {
         // remplace le texte dans le p timerDisplay
-        document.getElementById('timerDisplay').innerHTML = '00:' + sec;
+        const timerDisplay = document.getElementById('timerDisplay');
+        if (timerDisplay) {
+            timerDisplay.innerHTML = '00:' + sec;
+        }
         sec--;
         // si toutes les box ont étées cliquées dans le bon ordre
         if (nb == board.children.length + 1) {
@@ -72,17 +76,23 @@ for (let i = 1; i <= nbBoxes; i++) {
 
     // quand newbox est cliqué ...
     newbox.addEventListener("click", function () {
+        // ... si la bonne .box est cliquée
         if (i == nb) {
+            // on rajoute la classe box-clicked à newbox
             newbox.classList.add("box-clicked");
+            // et si toutes les bonnes .box ont été cliquées
             if (nb == board.children.length) {
                 board.querySelectorAll(".box").forEach(function (box) {
+                    // toutes les .box de #board recoivent la classe success
                     showReaction("success", box);
                 });
             }
-            // shuffleChildren(board);
+            //on incrémente nb pour la recherche suivante
             nb++;
         }
-        else if (i > nb) { // error
+        // si erreur
+        else if (i > nb) {
+            // on applique la classe .error à newbox
             showReaction("error", newbox);
             nb = 1;
             board.querySelectorAll(".box-clicked").forEach(function (validBox) {
@@ -90,7 +100,9 @@ for (let i = 1; i <= nbBoxes; i++) {
             });
             shuffleChildren(board);
         }
+        // si pas erreur ou success alors déjà cliqué
         else {
+            // on applique la classe .notice à newbox
             showReaction("notice", newbox);
         }
     });
@@ -98,6 +110,7 @@ for (let i = 1; i <= nbBoxes; i++) {
 
 retryBtn.addEventListener("click", () => {
     board.querySelectorAll(".box-clicked").forEach(function (validBox) {
+        // reset des classes pour enlever les effets CSS
         validBox.classList.remove("box-clicked");
         validBox.classList.remove("success");
     });
